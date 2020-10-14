c04435e8dbc2fc9d34b6dbc55b7cf56968e69628
jerry <kupull@naver.com>
2020-10-14 17:40:43 +0900

    yt

--- kwork.py
+++ kwork.py
@@ -1,18 +1,16 @@
-import os 
-os.getcwd()
 import time
 
-import requests
-from bs4 import BeautifulSoup
-from pymongo import MongoClient
-#from selenium import webdriver
+#import requests
+#from bs4 import BeautifulSoup
+#from pymongo import MongoClient
+from selenium import webdriver
 
-# driver = webdriver.Chrome("/home/jerry/Documents/Develop/chromedriver")
-# driver.get("https://www.k-startup.go.kr/common/announcement/announcementList.do?mid=30004&bid=701&searchAppAt=A")
+driver = webdriver.Chrome("/home/jerry/Documents/Develop/chromedriver")
+driver.get("https://www.work.go.kr/empInfo/indRev/indRevMain.do")
 
-url = "https://www.work.go.kr/empInfo/indRev/indRevMain.do"
-res = requests.get(url=url)
-soup = BeautifulSoup(res.content, features='lxml')
+# url = "https://www.work.go.kr/empInfo/indRev/indRevMain.do"
+# res = requests.get(url, verify=False)
+# soup = BeautifulSoup(res.content, features='lxml')
 
 
 # def DBinsert(data):
@@ -24,49 +22,22 @@ soup = BeautifulSoup(res.content, features='lxml')
 #         infor = kwdb.kwdbzips.insert_one(data)
 
 
+#회사명 완료
+# cp_list = [cors.text for cors in driver.find_elements_by_xpath("//a[@class='cp_name']")]
+# print(cp_list)
 
+#list1 > td:nth-child(3) > div > div > a
+
+#채용공고명 완료
+# ti_list = [titles.text for titles in driver.find_elements_by_tag_name("td:nth-child(3) > div > div > a")]
+# print(ti_list)
+
+#담당업무
+#jobContLine1
+dutes_list = [dutes.text for dutes in driver.find_elements_by_css_selector("td > div > div > p")]
+print(dutes_list)
 
 # def kwdb():
-
-# #기간 진배꺼
-# terms = soup.select('h4 > span[class*="ann_list_day"]')
-# # for term in terms:
-# #     print(term.text)
-
-
-
-
-# #카테고리 진배꺼
-# cates = soup.select('h4 > span[class*="ann_list_group"]')
-# # for cate in cates:
-# #     print(cate.text)
-
-
-
-
-
-
-# #li들 뽑아내기....진배옹의 가르침
-# li1_text, li2_text, li3_text= [], [], []
-# ul_tags = soup.find_all('ul', class_='ann_list_info m0')
-# for ul in ul_tags:
-#     li_tags = []
-#     for li in ul.find_all('li'):
-#         li_tags.append(li.text)
-#     li1_text.append(li_tags[0])
-#     li2_text.append(li_tags[1])
-#     li3_text.append(li_tags[2].split()[1])
-# #print(li1_text)
-
-
-# #타이틀 완료
-# titles= soup.select("li > h4 > a")
-# # for title in titles:
-# #     print(title.text)
-
-
-
-
 
 # for cate, term, title, li11_text, li22_text, li33_text in zip(cates, terms, titles, li1_text, li2_text, li3_text):
 #     #print(cate.text, term.text, title.text, li1_text, li2_text, li3_text)
@@ -76,7 +47,6 @@ soup = BeautifulSoup(res.content, features='lxml')
 #     print(data)
 #     #DBinsert(data)
 
-
-
 # if __name__ == "__main__":
 #     kwdb()
+driver.close()
